6ff961c091afb0fb74470c05a6ae100d1533ddf1
elmouradiAmine <elmouradi.amine98@gmail.com>
2020-08-02 20:31:53 +0100

    parse bug fidx

---
+++ build/precache-manifest.b28e3d54b4a900ab74e9fd235c326f43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957ea6294ced589ccc395663b8f58b40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f42b99a670e0044ee72",
+    "url": "/static/js/2.b8eacc07.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.b8eacc07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0aae8fee2399b9c9aa",
+    "url": "/static/js/main.174f642a.chunk.js"
+  },
+  {
+    "revision": "545623eea297b9a7927a",
+    "url": "/static/js/runtime-main.4fa04daa.js"
+  }
+]);
